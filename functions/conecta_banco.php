70310c2b69cefd018cf007d296b6ec2df65acf28
rafaelkasper <42684330+rafaelkasper@users.noreply.github.com>
2020-05-12 13:53:16 -0300

    Update conecta_banco.php

--- functions/conecta_banco.php
+++ functions/conecta_banco.php
@@ -1,9 +1,9 @@
 <?php
 
 //CONFIGURA A CONEXÃO COM O BD
-$servidor = "localhost";
-$usuario_bd = "root";
-$senha_bd = "180217";
+$servidor = "";
+$usuario_bd = "";
+$senha_bd = "";
 $banco = "hemolife";
 $con = new mysqli($servidor, $usuario_bd, $senha_bd, $banco);
 ini_set('display_errors', true);
